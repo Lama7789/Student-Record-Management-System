441ffa7e30247af5a38725856cac951599be96b2
Lama Alhujailan <431201810@qu.edu.sa>
2025-12-12 07:55:15 +0300

    Add files via upload

---
+++ Evaluatable/Evaluatable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public interface Evaluatable {
+    double calculateScore();
+    String getLevel();
+}    
+
