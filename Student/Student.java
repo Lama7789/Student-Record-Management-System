d36bfe9da1d3ff5cd8857e483461cfbe9c48e07b
Lama Alhujailan <431201810@qu.edu.sa>
2025-12-12 08:01:22 +0300

    Add files via upload

---
+++ Student/Student.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public abstract class Student implements Evaluatable {
+
+    protected String id;
+    protected String name;
+    protected double gpa;
+    protected String department;
+    private StudentAddress address;
+        
+   
+    public Student(String id, String name, double gpa, String department) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+        this.department = department;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    
+    public void setStudentAddress( StudentAddress address){
+        this.address=address;
+    }
+     public StudentAddress getStudentAddress(){
+         return address ;
+    }
+
+    public abstract void showDetails();
+    }
