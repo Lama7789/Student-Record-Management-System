7b5e9ab9c7a2e0ffddd9811509d4d46108059815
Lama Alhujailan <431201810@qu.edu.sa>
2025-12-12 08:08:44 +0300

    Update RegularStudent.java

--- RegularStudent/RegularStudent.java
+++ RegularStudent/RegularStudent.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author hp
- */
 public class RegularStudent extends Student {
 
     public RegularStudent(String id, String name, double gpa, String department) {
@@ -33,3 +25,4 @@ public class RegularStudent extends Student {
         return "Regular";
     }
 }
+
