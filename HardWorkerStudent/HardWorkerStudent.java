f80a4be42e1d7aa79ac567e9c34ec7c6b1ae059c
Lama Alhujailan <431201810@qu.edu.sa>
2025-12-12 08:05:28 +0300

    Add files via upload

---
+++ HardWorkerStudent/HardWorkerStudent.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class HardWorkerStudent extends Student {
+
+    private int studyHours;
+
+    public HardWorkerStudent(String id, String name, double gpa, String department,
+                             int studyHours) {
+        super(id, name, gpa, department);
+        this.studyHours = studyHours;
+    }
+
+    public void showDetails() {
+        System.out.println("----- Hard Worker Student -----");
+        System.out.println("ID: " + getId());
+        System.out.println("Name: " + getName());
+        System.out.println("GPA: " + getGpa());
+        System.out.println("Department: " + getDepartment());
+        if (getStudentAddress() != null) {
+        System.out.println("Campus: " + getStudentAddress().getcampus());
+        System.out.println("Room Number: " + getStudentAddress().getroomNumber());
+    }
+    }
+    
+    public double calculateScore() {
+        return getGpa() + (studyHours * 0.1);
+    }
+
+    public String getLevel() {
+        return "Hard Worker";
+    }
+    }
